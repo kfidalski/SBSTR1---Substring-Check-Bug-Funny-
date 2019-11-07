4a4f9c7bce6847cb1c3fec95fc9a13923769194f
kfidalski <48648696+kfidalski@users.noreply.github.com>
2019-11-07 11:04:23 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBSTR1___Substring_Check__Bug_Funny_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = "";
+            string[] data = new string[input.Length];
+
+
+
+            string a = "";
+            string b = "";
+
+
+            int n = 24;
+            int[] results = new int[n];
+
+            for(int i = 0; i < n; i++)
+            { 
+                input = Console.ReadLine();
+                data = input.Split(' ');
+                a = data[0];
+                b = data[1];
+
+                if (a.Length <= 10 && b.Length <=5)
+                {
+                    results[i] = CheckString(a, b);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+
+
+
+
+                Console.ReadKey();
+        }
+
+        public static int CheckString(string a ,string b)
+        {
+
+            int result=  a.IndexOf(b, 0, a.Length);
+
+            if(result >= 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+
+        }
+
+    }
+}
